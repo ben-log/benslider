c54605f511e8929eb92dde0056226630f9a5391d
toolazytalker <toolazytalker@gmail.com>
2020-08-04 10:49:44 +0900

    asciiArt, Version, Author 주석 추가

--- js/benslider.js
+++ js/benslider.js
@@ -1,3 +1,16 @@
+/**
+ * 
+ *     ____                  ___     __         
+ *    / __ )___  ____  _____/ (_)___/ /__  _____
+ *   / __  / _ \/ __ \/ ___/ / / __  / _ \/ ___/
+ *  / /_/ /  __/ / / (__  ) / / /_/ /  __/ /    
+ * /_____/\___/_/ /_/____/_/_/\__,_/\___/_/     
+ *
+ * Version: 0.0.1
+ * Author: HyoGyeong Ryu
+ * Repo: http://github.com/ben-log/benslider
+ * 
+ */
 (function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
     typeof define === 'function' && define.amd ? define(factory) :
